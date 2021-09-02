f5c3c51addd3ca01a374efb981e49872b5ae1cd6
J C <Raghavendra.JC@team.telstra.com>
2021-09-02 15:18:31 +0530

    React front end for mytelstra

---
+++ mytelstra/src/App.js
@@ -0,0 +1,14 @@
+//import logo from './logo.svg';
+import './App.css';
+//import SignIn from './login/LogIn';
+import ViewPlans from './Broadband/viewPlans';
+
+function App() {
+  return(
+    <div>
+      <ViewPlans />
+    </div>
+  );
+}
+
+export default App;
